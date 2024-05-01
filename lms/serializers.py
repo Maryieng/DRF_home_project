b72f507c8bc691f6caa565f4974b08060c62088a
Mary <kass.o@yandex.ru>
2024-05-01 14:53:02 +0300

    CRUD for users, JWT authorizations, Create a group of moderators, users edit only their data, docstrings added

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,13 +4,23 @@ from lms.models import Well, Lesson
 
 
 class LessonSerializers(serializers.ModelSerializer):
+    """ Сериализатор для модели урока """
 
     class Meta:
         model = Lesson
         fields = '__all__'
 
+    def create(self, validated_data):
+        """ привязка пользователя как владельца к новому уроку """
+        user = self.context['request'].user
+        lesson = Lesson(**validated_data)
+        lesson.owner = user
+        lesson.save()
+        return lesson
+
 
 class WellSerializers(serializers.ModelSerializer):
+    """Сериализатор для модели курса """
     lesson_list = LessonSerializers(source='lesson_set', many=True, read_only=True)
     num_lessons = serializers.SerializerMethodField()
 
@@ -19,4 +29,13 @@ class WellSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_num_lessons(self, well):
+        """ счетчика просмотров """
         return Lesson.objects.filter(well=well).count()
+
+    def create(self, validated_data):
+        """ привязка пользователя как владельца к новому курсу """
+        user = self.context['request'].user
+        course = Well(**validated_data)
+        course.owner = user
+        course.save()
+        return course
--- users/views.py
+++ users/views.py
@@ -1,27 +1,55 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from users.models import User, Payments
+from users.permissions import IsModerator
 from users.serializers import UserSerializer, PaymentsSerializer
 
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """ вывод информации о пользователях """
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
-class UserListView(generics.ListAPIView):
+class UserAPIListView(generics.ListAPIView):
+    """Просмотр всех пользователей"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Создание пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """Изменение пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsModerator]
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    """Удаление пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsModerator]
 
 
 class PaymentsCreate(generics.CreateAPIView):
+    """ создание платежа """
     serializer_class = PaymentsSerializer
 
 
 class PaymentListAPIView(generics.ListAPIView):
+    """ список всех платежей """
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
     filter_backends = [DjangoFilterBackend, OrderingFilter]
